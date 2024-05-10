962ddfd9fc5fdeace8de463d3227aa5d5488412b
raziqueansari1991 <raziqueansari711@gmail.com>
2024-05-10 07:36:43 +0530

    create public

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,13 @@ resource "aws_vpc" "TCS-Sales-VPC" {
     Name = "Sales_VPC"
   }
 }
+# public subnet
+resource "aws_subnet" "tcs-subnet" {
+  vpc_id     = aws_vpc.TCS-Sales-VPC.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "subnet-tcs"
+  }
+}
